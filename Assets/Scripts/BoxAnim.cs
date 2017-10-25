42ffb31e5f0351582e5cfbbf518f901659a384fd
OISHI-PC\oishi <kouta.oishi455@gmail.com>
2017-10-25 19:27:05 +0900

    Git Hub Up

---
+++ Assets/Scripts/BoxAnim.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BoxAnim : MonoBehaviour {
+	private Animator animator;
+	private string[] randomAnimationString = {"Emotion0","Emotion1","Emotion2","Emotion3","Emotion4"} ;
+
+	private void Start () {
+		animator = GetComponent<Animator>();
+		RandomAnim ();
+	}
+
+	private void RandomAnim ()
+	{
+		string randomEmotion = randomAnimationString [Random.Range (0, 5)];
+
+		EmotionText (randomEmotion);
+		
+		animator.Play (randomEmotion,0, 0.0f);
+	}
+
+	/// アニメーション名表示
+	[SerializeField]Text emotionText;
+	private void EmotionText (string randomEmotion)
+	{
+		emotionText.text = randomEmotion;
+	}
+}
